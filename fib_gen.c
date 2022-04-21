f9dfe979eca99ddd9b10c031797e9e6c97733949
Geode42 <99060684+Geode42@users.noreply.github.com>
2022-04-21 10:01:44 -0400

    Update fib_gen.c

--- fib_gen.c
+++ fib_gen.c
@@ -5,24 +5,22 @@ int main() {
 	printf("Amount of fibonacci numbers to generate: ");
 	scanf("%u", &amount);
 
-	if (amount == 0) {
-		return 0;
-	}
-	if (amount >= 1) {
-		printf("%i\n", 0);
-	}
-	if (amount >= 2) {
-		printf("%i\n", 1);
-	}
-
 	unsigned long long num1 = 0, num2 = 1, temp_num;
 
-	for (short i = 2; i <= amount; i++) {
+	if (amount >= 1) {
+		printf("%llu ", num1);
+	}
+	if (amount >= 2) {
+		printf("%llu ", num2);
+	}
+
+	for (unsigned short i = 2; i <= amount; i++) {
 		temp_num = num2;
 		num2 = num1 + num2;
 		num1 = temp_num;
-		printf("%llu\n", num2);
+		printf("%llu ", num2);
 	}
 
+	printf("\n");
 	return 0;
 }
